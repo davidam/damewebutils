579f2c1409848700f5361398fbbfefd787f409b3
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-11-19 19:35:47 +0100

    imgs.py: adapting source from libremanuals to a generic way

--- imgs.py
+++ imgs.py
@@ -17,47 +17,48 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with damewebutils; see the file COPYING.  If not, write to
+# along with webutils; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
 import requests
 from lxml import html
-from pprint import pprint
 import os,re
+import argparse
 
-start_url = 'http://127.0.0.1:5000'
-pages = ['/index.html', '/elisp-es.html', '/org-mode.html', '/gcc.html', '/r.html', '/python.html', '/bash.html', '/vim.html', '/debian.html', '/gimp.html', '/acerca.html', '/libros.html', '/perspectiva-libertaria.html', '/faq-freedoc.html']
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="url to analize broken links")
+args = parser.parse_args()
+print(args.url)
 
+start_url = args.url
+response = requests.get(start_url)
+tree = html.fromstring(response.text)
+imgs = tree.cssselect('img')  # or tree.xpath('//a')
 
-for p in pages:
-    response = requests.get(start_url + p)
-    tree = html.fromstring(response.text)
-    imgs = tree.cssselect('img')  # or tree.xpath('//a')
-
-    out = []
-    for i in imgs:
+out = []
+for i in imgs:
     # we use this if just in case some <a> tags lack an href attribute
-        if 'src' in i.attrib:
-            out.append(i.attrib['src'])
+    if 'src' in i.attrib:
+        out.append(i.attrib['src'])
 
-    absoluteurls = []
-    for o in out:
-        if 'http' in o:
-            absoluteurls.append(o)
-        else:
-            o = start_url + o
-            absoluteurls.append(o)
+absoluteurls = []
+for o in out:
+    if 'http' in o:
+        absoluteurls.append(o)
+    else:
+        o = start_url + o
+        absoluteurls.append(o)
 
-    buggyurls = []
-    for l in absoluteurls:
-        print(l)
-        try:
-            r = requests.get(l)
-            r.raise_for_status()
-            print(r.status_code)
-        except:
-            buggyurls.append(l)
-            print("Error %s" % r.status.code)
-    print("Imgs with possible troubles in %s" % l)
-#    print(buggyurls)
+buggyurls = []
+for l in absoluteurls:
+    print(l)
+    try:
+        r = requests.get(l)
+        r.raise_for_status()
+        print(r.status_code)
+    except:
+        buggyurls.append(l)
+        print("Error %s" % r.status.code)
+
+print("Imgs with possible troubles in %s" % l)
