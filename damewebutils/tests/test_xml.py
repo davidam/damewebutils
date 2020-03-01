c53efc29129b5741b766992bf82d8d2ea586a816
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-03-01 16:27:31 +0100

    damewebutils/files/items.xml, damewebutils/tests/test_xml.py: creation

---
+++ damewebutils/tests/test_xml.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with damepandas; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from unittest import TestCase
+from xml.dom import minidom
+
+class TestXml(TestCase):
+
+    def test_minidom(self):
+        xmldoc = minidom.parse('files/items.xml')
+        itemlist = xmldoc.getElementsByTagName('item')
+        self.assertEqual(len(itemlist), 4)
+        l = []
+        for s in itemlist:
+            l.append(s.attributes['name'].value)
+        self.assertEqual(l, ['item1', 'item2', 'item3', 'item4'])
