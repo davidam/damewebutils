0712175ef07befc652e9a5d1bee3ac057d5a1860
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-03-01 16:53:57 +0100

    testsbycommands.sh: comment davidam test temporally busy, add test for template.py

--- testsbycommands.sh
+++ testsbycommands.sh
@@ -20,21 +20,30 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-python3 dameurls.py http://www.davidam.com > files/tests/davidam-index-$(date "+%Y-%m-%d-%H").txt
+# python3 dameurls.py http://www.davidam.com > damewebutils/files/tests/davidam-index-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/davidam-index.txt files/tests/davidam-index-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-then
-    echo "dameurls.py with davidam.com test is failing"
-else
-    echo "dameurls.py with davidam.com test is ok"
-fi
+# if ! cmp damewebutils/files/tests/davidam-index.txt damewebutils/files/tests/davidam-index-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+# then
+#     echo "dameurls.py with davidam.com test is failing"
+# else
+#     echo "dameurls.py with davidam.com test is ok"
+# fi
 
 
-python3 dameimgs.py http://www.damegender.net > files/tests/damegender-imgs-$(date "+%Y-%m-%d-%H").txt
+python3 dameimgs.py http://www.damegender.net > damewebutils/files/tests/damegender-imgs-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/damegender-imgs.txt files/tests/damegender-imgs-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp damewebutils/files/tests/damegender-imgs.txt damewebutils/files/tests/damegender-imgs-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
     echo "dameimgs.py with damegender.net test is failing"
 else
     echo "dameimgs.py with damegender.net test is ok"
+fi
+
+python3 template.py > damewebutils/files/tests/template-$(date "+%Y-%m-%d-%H").txt
+
+if ! cmp damewebutils/files/tests/template.txt damewebutils/files/tests/template-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+then
+    echo "template.py is failing"
+else
+    echo "template.py is ok"
 fi
